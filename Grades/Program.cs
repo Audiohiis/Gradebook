f9b4a809598596fb87f011e48754209f12078bc2
Hedi <the.hedi@hotmail.com>
2017-11-05 17:59:50 +0200

    Calculate average, highest and lowest grades

---
+++ Grades/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Grades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GradeBook book = new GradeBook();
+            book.AddGrade(45.8f);
+            book.AddGrade(50);
+            book.AddGrade(91);
+
+            GradeStatistics stats = book.ComputeStatistics();
+
+            Console.WriteLine(stats.AverageGrade);
+            Console.WriteLine(stats.HighestGrade);
+            Console.WriteLine(stats.LowestGrade);
+
+        }
+    }
+}
